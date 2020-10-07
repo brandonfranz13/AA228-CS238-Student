7d30efa82f29afe21d31f54fa59d8d2c14070d0a
brandonfranz13 <brandonfranz13@gmail.com>
2020-10-07 17:15:08 -0500

    project1 started

---
+++ project1/Project1_BayesianStructureLearning.py
@@ -0,0 +1,18 @@
+
+class BayesianStructureLearning()
+
+    def __init__(self)
+    
+    def scoreGraph(self)
+        print("Scoring Complete")
+        
+    def graphSearch(self)
+        print("Graph Search Complete\n")
+        
+    def solve(self)
+    
+        while True:
+            graph = self.graphSearch()
+            score = self.scoreGraph()
+            print("Solve Complete\n")
+            break
