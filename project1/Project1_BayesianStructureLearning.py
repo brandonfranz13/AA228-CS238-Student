12cc20061b3ecbc2b5e637d6e3ea41fd83e6f603
Brandon Riley Franz <franzb@rice16.stanford.edu>
2020-10-10 09:43:49 -0700

    printing, wip

--- project1/Project1_BayesianStructureLearning.py
+++ project1/Project1_BayesianStructureLearning.py
@@ -1,16 +1,13 @@
 
-class BayesianStructureLearning():
+class BayesianStructureLearning:
 
-    def __init__(self):
-    
     def scoreGraph(self):
         print("Scoring Complete")
-        
+
     def graphSearch(self):
         print("Graph Search Complete\n")
-        
+
     def solve(self):
-    
         while True:
             graph = self.graphSearch()
             score = self.scoreGraph()
