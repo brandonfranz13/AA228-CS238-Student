41311775c6871d3c3a94fbdd1f68af49adc23f33
brandonfranz13 <brandonfranz13@gmail.com>
2020-10-25 21:29:21 -0500

    wip

--- project2/RL.py
+++ project2/RL.py
@@ -8,19 +8,19 @@ def importCSV(filename):
     return contents.columns, contents.to_numpy()
     
     
-class SmallRL()
+class SmallRL():
     '''
     Description of Data Set
     10 x 10 grid world (100 states) with 4 actions. Actions are 1: left, 2: right, 3: up, 4: down. The discount factor is 0.95.
     '''
 
-    def __init__(self)
+    def __init__(self):
         _, sarsp = importCSV("small.csv")
         self.s = sarsp[:,0]
         self.a = sarsp[:,1]
         self.r = sarsp[:,2]
         self.sp = sarsp[:,3]
         
-    def solve(self)
+    def solve(self):
         writePolicy("small.policy", self.a)
         print "Policy Written to File"
