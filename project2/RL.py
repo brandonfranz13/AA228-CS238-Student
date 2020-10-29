fd0ec29e553e3320a4f40d6799f53e2409e21a43
brandonfranz13 <brandonfranz13@gmail.com>
2020-10-29 14:15:16 -0500

    wip

--- project2/RL.py
+++ project2/RL.py
@@ -1,15 +1,8 @@
-import pandas as pd
-import numpy as np
+from utils import *
 
-def writePolicy(filename, actions):
-    np.savetxt(filename, actions, fmt='%i', delimiter=",")
-    
-def importCSV(filename):
-    contents = pd.read_csv(filename)
-    return contents.columns, contents.to_numpy()
-    
-    
-class SmallRL():
+# MDP = namedtuple("MDP", "discount States Actions Transition Reward")
+ 
+class SmallRL(discount=0.95, max_iterations=100):
     '''
     Description of Data Set
     10 x 10 grid world (100 states) with 4 actions. Actions are 1: left, 2: right, 3: up, 4: down. The discount factor is 0.95.
@@ -21,14 +14,30 @@ class SmallRL():
         self.a = sarsp[:,1]
         self.r = sarsp[:,2]
         self.sp = sarsp[:,3]
+        self.n_states = 100
+        self.n_actions = 4
+        self.k = max_iterations
         
-    def explore(self):
-        return True
+        self.N = np.zeros(n_states*n_actions*len(self.s)) # S x A x transitions
+        self.rho = np.zeros(n_states*n_actions) # S x A
         
-    def learn(self):
+    def single2multi_index(single, dims):
+        return np.unravel_index(single, dims)
+    
+    def multi2single_index(multi, dims):
+        return np.ravel_multi_index(multi, dims)
+    
+    def learnMaxLike(self):
+        # Count the occurrence of (s,a,sp) transitions
+        for i in range(len(self.s)):
+            index = self.multi2single_index(
+        # Accumulate the reward for transitions from (s, a)
         return True
     
-    def iterate(self):
+    def valueIteration(self):
+        U = np.zeros(len(self.s))
+        for i in range(k):
+            U = [backup(
         return True
     
     def restart(self):
--- project2/test_RL.py
+++ project2/test_RL.py
@@ -1,5 +1,9 @@
 import RL
 
+
+
+print "Test Solve"
 small = RL.SmallRL()
 
 small.solve()
+print "Test Solve Complete"
